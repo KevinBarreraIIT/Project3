7f05de971757f44dda6787f4cf1b9edee538dd17
KevinBarreraIIT <60115269+KevinBarreraIIT@users.noreply.github.com>
2020-04-20 17:37:36 -0500

    added a marker, info window, and changed the map into darkmode

--- js/script.js
+++ js/script.js
@@ -1,6 +1,113 @@
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 41.919413, lng: -87.7604902},
-        zoom: 15
+    var myLatLng = {lat: 41.919413, lng: -87.760902}
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 15,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
     });
+
+    var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Prosser Career Academy</h1>'+
+    '<div id="bodyContent">'+
+    '<p><strong>Prosser Career Academy</strong>, the high school I graduated from, '+
+    'is a vocational CPS highschool located on the Northwest side of Chicago, by the intersection '+
+    'of Grand and Central. Other than being the high school that famous serial killer John Wayne Gacy '+
+    'once attended, there is not much else to say. Prosser has bad air conditioning, and I will never '+
+    'forgive them for taking away our vending machines.</p> '+
+    '</div>'+
+    '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+    content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Prosser Career Academy'
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+     });
     }
